2c01de4dc5571ef38d1477601fa1f1816e08ff5e
Seundede <omadoyedede0@gmail.com>
2025-01-03 11:13:50 +0100

    chore: clerk/nextjs sdk installation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-    Prime pick
+    <div className="p-4">
+    <Button>Click</Button>
     </div>
   );
 }
